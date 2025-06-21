eb9d6b74adb2ac4db6b03e3e4d7c885bdf20d985
jotre17 <eadjoaotrevisan932@gmail.com>
2025-06-20 23:39:04 -0300

    Signed-off-by: jotre17 <eadjoaotrevisan932@gmail.com>

--- SGHSS1.0/SGHSS1.0/core/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import UnidadeSaude
-
-class UnidadeSaudeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = UnidadeSaude
-        fields = '__all__'
