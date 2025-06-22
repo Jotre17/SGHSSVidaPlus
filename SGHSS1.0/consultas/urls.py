6bcbea88bf702df179342d05614a3ddcd24495f5
Jotre17 <162373558+Jotre17@users.noreply.github.com>
2025-06-22 15:20:56 -0300

    Delete SGHSS1.0 directory

--- SGHSS1.0/consultas/urls.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import routers
-from .views import ConsultaViewSet, AgendamentoViewSet
-
-router = routers.DefaultRouter()
-router.register(r'consultas', ConsultaViewSet)
-router.register(r'agendamentos', AgendamentoViewSet)
-
-urlpatterns = router.urls
