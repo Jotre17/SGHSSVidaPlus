3ae735e34b8e95ff253fe5796181c0f00c5628a7
Jotre17 <162373558+Jotre17@users.noreply.github.com>
2025-06-22 15:13:25 -0300

    Delete SGHSS1.0 directory

--- SGHSS1.0/pacientes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Paciente
-
-@admin.register(Paciente)
-class PacienteAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'cpf', 'sexo', 'unidade_saude')
-    search_fields = ('nome', 'cpf')
-
-
