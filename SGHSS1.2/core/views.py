46a584d8e59537525024ba09bbd000f50fd75c64
Jotre17 <162373558+Jotre17@users.noreply.github.com>
2025-06-22 15:20:15 -0300

    Delete SGHSS1.2 directory

--- SGHSS1.2/core/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import UnidadeSaude
-from .serializers import UnidadeSaudeSerializer
-
-
-class UnidadeSaudeViewSet(viewsets.ModelViewSet):
-    queryset = UnidadeSaude.objects.all()
-    serializer_class = UnidadeSaudeSerializer
