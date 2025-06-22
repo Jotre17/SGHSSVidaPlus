189d70c12572a2db61e99075318025115837b811
Jotre17 <162373558+Jotre17@users.noreply.github.com>
2025-06-22 15:20:24 -0300

    Delete SGHSS1.1 directory

--- SGHSS1.1/pacientes/urls.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import routers
-from .views import PacienteViewSet
-
-router = routers.DefaultRouter()
-router.register(r'pacientes', PacienteViewSet)
-
-urlpatterns = router.urls
